a006bca4a6637fe7fe1316507067aaeedb5c051e
David Arrazola <arrazola.dvd@gmail.com>
2026-02-22 17:52:09 -0500

    ADD functionality to get array of all employees through GET/ request

--- api/employees.js
+++ api/employees.js
@@ -1,3 +1,9 @@
 import express from "express";
+import { getEmployees } from "#db/queries/employees";
 const router = express.Router();
 export default router;
+
+router.get("/", async (req, res) => {
+  const employees = await getEmployees();
+  res.send(employees);
+});
